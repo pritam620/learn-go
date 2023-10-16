d9284d2e093bf2fa6b8b2212c8b6036a97cab708
Pritam Kumar <pritam.kumar@truecaller.com>
2023-10-16 16:00:37 +0200

    Pointers: Add Bitcoint type with stringer (%s)

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,13 +1,25 @@
 package pointers
 
+import "fmt"
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (wallet *Wallet) Deposit(amount int) {
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type stringer interface {
+	String() string
+}
+
+func (wallet *Wallet) Deposit(amount Bitcoin) {
 	wallet.balance = wallet.balance + amount
 }
 
-func (wallet Wallet) Balance() int {
+func (wallet Wallet) Balance() Bitcoin {
 	return wallet.balance
 }
