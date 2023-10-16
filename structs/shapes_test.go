2cfe7038b3e0ca00cf8a8af6803f6f7a935ea98c
Pritam Kumar <pritam.kumar@truecaller.com>
2023-10-16 13:09:39 +0200

    Struct: Refactor test, add table driven test

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,20 +15,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("Expected %g but got %g", tt.want, got)
 		}
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
 }
